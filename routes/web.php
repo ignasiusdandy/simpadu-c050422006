3a02feb16091112d302e4e59a0e6b1e6b7abd0d0
ignasiusdandy <ignasiusdandy123@gmail.com>
2023-11-06 00:07:34 +0800

    integrasi backend stisla

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view('pages.auth.auth-login', ['type_menu' => '']);
+});
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('home', function() {
+    return view('pages.app.dashboard-simpadu', ['type_menu' => '']);
+    })->name('home');
 });
